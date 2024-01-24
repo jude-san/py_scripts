a9860e87add5a85e4af515d6543ce5ce8c54bc81
jude-san <judekhay@gmail.com>
2024-01-24 19:55:30 +0000

    review

--- open_file.py
+++ open_file.py
@@ -1,7 +1,9 @@
+# creating(if does not exist) and reading a file 
 f = open('my_text_script.txt', 'w')
 f.write('Hello Wolrd!!')
 f.close()
 
+# using 'with' function so that i do not need to use close() for every code.
 
 with open('my_text_script.txt', 'r') as f:
     file_data = f.read()
